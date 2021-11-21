35c98dad2dac454a368fa67a87082b4272456336
Henok <17439057+henokalem@users.noreply.github.com>
2021-11-20 19:34:58 -0500

    tailwind setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
-import '../styles/globals.css'
+import 'tailwindcss/tailwind.css'
 import type { AppProps } from 'next/app'
-import MainLayout from '../components/layouts/main_layout';
+import MainLayout from '../components/layouts/main_layout'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return ( 
