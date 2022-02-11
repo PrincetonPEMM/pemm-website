45a206d630861a487363edd9e5d4abde2746eee5
issacjohannli <issacjohannli@gmail.com>
2022-02-11 16:29:06 +0000

    implemented initial carousel

--- pages/stories.tsx
+++ pages/stories.tsx
@@ -1,4 +1,21 @@
 import type { NextPage } from 'next'
+import React from 'react';
+import ImageGallery from 'react-image-gallery';
+
+const IMAGES = [
+  {
+    original: 'https://picsum.photos/id/1018/1000/600/',
+    thumbnail: 'https://picsum.photos/id/1018/250/150/',
+  },
+  {
+    original: 'https://picsum.photos/id/1015/1000/600/',
+    thumbnail: 'https://picsum.photos/id/1015/250/150/',
+  },
+  {
+    original: 'https://picsum.photos/id/1019/1000/600/',
+    thumbnail: 'https://picsum.photos/id/1019/250/150/',
+  },
+];
 
 const Stories: NextPage = () => {
   return (
@@ -6,6 +23,11 @@ const Stories: NextPage = () => {
         <h1>
           Stories
         </h1>
+        <div className = 'grid grid-cols-4'>
+          <div>
+            <ImageGallery items={IMAGES} />
+          </div>
+        </div>
     </div>
   )
 }
