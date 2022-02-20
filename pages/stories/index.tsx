b2d429092300cf59515cac813d414d6926fd5f9c
Henok <17439057+henokalem@users.noreply.github.com>
2022-02-20 13:37:04 -0500

    Getting story list from api and basic UI

--- pages/stories/index.tsx
+++ pages/stories/index.tsx
@@ -1,34 +1,48 @@
-import type { NextPage} from 'next'
+import type { NextPage, GetServerSideProps, InferGetServerSidePropsType} from 'next'
 import React from 'react';
+import type {Stories} from '../../components/types/stories';
+import axios from 'axios';
+import StoryHighlightsComponent from '../../components/elements/storyHighlightsComponent';
 
-const Stories: NextPage = () => {
-    //TODO: Update this page to get list of all canonical ids from db and render links to those stories
-  const website: string = "https://main.dtma86dor8lfe.amplifyapp.com";
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  try {
+    const res = await axios(process.env.REACT_APP_API + 'stories/');
+    const stories: Stories[] = await res.data;
+    return {
+      props: {
+        data: {
+          stories: stories
+        }
+      }
+    }
+  } catch {
+      return {
+        props: {
+          data: {
+            stories: []
+          }
+        }
+      }
+  }
+}
+
+const StoriesPage: NextPage = ({ data }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
   return (
     <div>
       <h1>
         Stories
       </h1>
       <div className="flex space-x-10 flex-wrap overflow-hidden">
-        <ul>
-          <li><a href={website + "/stories/7"}>
-            <button type="button" className="m-2 w-full h-12 px-6 text-indigo-100 transition-colors duration-150 bg-indigo-700 rounded-lg focus:shadow-outline hover:bg-indigo-800">Story Number: 7</button>
-            </a></li>
-          <li><a href={website + "/stories/13"}>
-            <button type="button" className="m-2 w-full h-12 px-6 text-indigo-100 transition-colors duration-150 bg-indigo-700 rounded-lg focus:shadow-outline hover:bg-indigo-800">Story Number: 13</button></a></li>
-          <li><a href={website + "/stories/19"}>
-            <button type="button" className="m-2 w-full h-12 px-6 text-indigo-100 transition-colors duration-150 bg-indigo-700 rounded-lg focus:shadow-outline hover:bg-indigo-800">Story Number: 19</button>
-          </a></li>
-          <li><a href={website + "/stories/27"}>
-            <button type="button" className="m-2 w-full h-12 px-6 text-indigo-100 transition-colors duration-150 bg-indigo-700 rounded-lg focus:shadow-outline hover:bg-indigo-800">Story Number: 27</button>
-            </a></li>
-          <li><a href={website + "/stories/33"}>
-            <button type="button" className="m-2 w-full h-12 px-6 text-indigo-100 transition-colors duration-150 bg-indigo-700 rounded-lg focus:shadow-outline hover:bg-indigo-800">Story Number: 33</button>
-            </a></li>
-        </ul>
+          {data.stories && data.stories.map(function(story: Stories, i: any){
+            return (
+              <div key={i}>
+                <StoryHighlightsComponent story={story}/>
+              </div>
+            )
+        })}
       </div>
     </div>
   )
 }
 
-export default Stories
+export default StoriesPage
