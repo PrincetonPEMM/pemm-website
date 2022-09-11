d1fcc15c12434fc1ec5eb08efe38f9d8643e17ed
Issac Li <67703799+issacjohannli@users.noreply.github.com>
2022-09-11 19:47:14 +0100

    Added maps.tsx - Maps page

---
+++ src/pages/maps.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from 'next'
+import EmbeddedMap from '../components/elements/embeddedMap';
+
+const Maps: NextPage = () => {
+  return (
+    <div>
+        <script type="text/javascript" src="https://public.tableau.com/javascripts/api/tableau-2.min.js"></script>
+        <EmbeddedMap />
+    </div>
+  )
+}
+
+export default Maps
