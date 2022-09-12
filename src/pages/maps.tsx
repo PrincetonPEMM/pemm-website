3708bf1cc577cb563967e3b839836b1e3e1c8abd
Henok Alem <17439057+henokalem@users.noreply.github.com>
2022-09-12 16:13:55 -0400

    Reverting script import location because next doesn't like me

--- src/pages/maps.tsx
+++ src/pages/maps.tsx
@@ -1,9 +1,11 @@
 import type { NextPage } from 'next'
 import EmbeddedMap from '../components/elements/embeddedMap';
+import Script from 'next/script';
 
 const Maps: NextPage = () => {
   return (
     <div>
+      <Script src="https://public.tableau.com/javascripts/api/tableau-2.min.js"/>
       <EmbeddedMap />
     </div>
   )
