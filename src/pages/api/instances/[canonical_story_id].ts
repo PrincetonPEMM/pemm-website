7623c5d4f479f54a62fa55ba82214f866ec1998c
Henok Alem <17439057+henokalem@users.noreply.github.com>
2022-09-13 22:28:40 -0400

    Updating return type of instances api

--- src/pages/api/instances/[canonical_story_id].ts
+++ src/pages/api/instances/[canonical_story_id].ts
@@ -1,5 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
-import type {Stories} from '../../../components/types/stories';
+import type {Instances} from '../../../components/types/instances';
 import AWS from 'aws-sdk';
 import Lambda from "aws-sdk/clients/lambda";
 import {AWSError} from 'aws-sdk/lib/error';
@@ -28,21 +28,21 @@ const lambda = new AWS.Lambda({
 
 export default function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Stories>
+  res: NextApiResponse<Instances[]>
 ) {
     const { canonical_story_id } = req.query;
     if (typeof(canonical_story_id) == "string") {
         const instances = getStoryInstanceByCanonicalId(canonical_story_id);
         instances.then((data: any) => {
-            let story : Stories = JSON.parse(data).body;
-            res.status(200).send(story);
+            let instances : Instances[] = JSON.parse(data).body;
+            res.status(200).send(instances);
             res.end();
         }).catch(() => {
-            res.status(500).send({});
+            res.status(500).send([]);
             res.end();
         });
     } else {
-        res.status(500).send({});
+        res.status(500).send([]);
         res.end();
     }
 }
