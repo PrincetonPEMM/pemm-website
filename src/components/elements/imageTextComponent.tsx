2db78684c27f20962c5fc521713f01f035c5bdee
Henok Alem <17439057+henokalem@users.noreply.github.com>
2022-09-18 16:17:39 -0400

    Updating accordion stuff

--- src/components/elements/imageTextComponent.tsx
+++ src/components/elements/imageTextComponent.tsx
@@ -5,33 +5,25 @@ import Button  from '@mui/material/Button';
 
 export const ImageTextComponent = (props: any) => {
     return (
-      <>
-        <div className="relative overflow-hidden">
-            <Image src={props.imageUrl} width={600} height={1000} className="object-cover" alt="Painting From a Manuscript" />
-            <div className="absolute py-2.5 bottom-64 inset-x-0 leading-4 flex flex-col">
-                <Link href={props.link}>
-                    <Button className="block">
-                        <Typography variant="h5" color="secondary">
-                            Featured
-                        </Typography>
-                    </Button>
-                </Link>
-                <Link href={props.link}>
-                    <Button className="block">
-                        <Typography variant="h3" color="secondary">
-                            {props.title}
-                        </Typography>
-                    </Button>
-                </Link>
-                <Link href={props.link}>
-                    <Button className="block">
-                        <Typography variant="subtitle1" color="secondary">
-                            {props.description}
-                        </Typography>
-                    </Button>
-                </Link>
-            </div>
+    <div className="relative overflow-hidden w-1/3" onClick={props.accordionHandler}>
+        <Image src={props.imageUrl} width={600} height={1000} className="object-cover" alt="Painting From a Manuscript" />
+        <div className="absolute py-2.5 bottom-64 inset-x-0 leading-4 flex flex-col">
+        <Button className="block">
+            <Typography className="block" variant="h5" color="secondary">
+                Featured
+            </Typography>
+        </Button>
+            <Button className="block">
+                <Typography variant="h3" color="secondary">
+                    {props.title}
+                </Typography>
+            </Button>
+            <Button className="block">
+                <Typography variant="subtitle1" color="secondary">
+                    {props.description}
+                </Typography>
+            </Button>
         </div>
-      </>
+    </div>
     );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ import Box from '@mui/material/Box';
 import {ImageTextComponent} from '../components/elements/imageTextComponent';
 import {FeaturedContentComponent} from '../components/elements/featuredContentComponent';
 import { Accordion, AccordionSummary, AccordionDetails } from '@mui/material';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import { useState } from 'react';
 
 const STORY_IMAGE = "/stories.jpeg";
 const STORY_TITLE = "Stories";
@@ -23,59 +23,89 @@ const PAINTINGS_TITLE = "Paintings";
 const PAINTINGS_DESCRIPTION = ""; // FILL IN
 const PAINTINGS_LINK="/paintings";
 const PAINTINGS_BACKGROUND_COLOR = ""; // FILL IN
-const PAINTINGS_CONTENT = []; // FILL IN
+const PAINTINGS_CONTENT = [
+  {'title' : 'Healed Daughter', 'id': '111', 'illustration': '/stories.jpeg'},
+  {'title' : 'Thirsty Dog', 'id': '236', 'illustration': '/stories.jpeg'},
+  {'title': 'Ethiopian King', 'id': '319', 'illustration': '/stories.jpeg'},
+]; // FILL IN
 
 const MANUSCRIPTS_IMAGE = "/manuscripts.jpeg";
 const MANUSCRIPTS_TITLE = "Manuscripts";
 const MANUSCRIPTS_DESCRIPTION = ""; // FILL IN
 const MANUSCRIPTS_LINK="/manuscripts";
 const MANUSCRIPTS_BACKGROUND_COLOR = ""; // FILL IN
-const MANUSCRIPTS_CONTENT = []; // FILL IN
+const MANUSCRIPTS_CONTENT = [
+  {'title' : 'Healed Daughter', 'id': '111', 'illustration': '/stories.jpeg'},
+  {'title' : 'Thirsty Dog', 'id': '236', 'illustration': '/stories.jpeg'},
+  {'title': 'Ethiopian King', 'id': '319', 'illustration': '/stories.jpeg'},
+]; // FILL IN
+
+const STORY_DATA = [
+  STORY_IMAGE, STORY_TITLE, STORY_DESCRIPTION, STORY_LINK,STORY_BACKGROUND_COLOR, STORY_CONTENT];
+const MANUSCRIPTS_DATA = [
+  MANUSCRIPTS_IMAGE, MANUSCRIPTS_TITLE, MANUSCRIPTS_DESCRIPTION, MANUSCRIPTS_LINK, MANUSCRIPTS_BACKGROUND_COLOR, MANUSCRIPTS_CONTENT];
+const PAINTINGS_DATA = [
+  PAINTINGS_IMAGE, PAINTINGS_TITLE, PAINTINGS_DESCRIPTION, PAINTINGS_LINK, PAINTINGS_BACKGROUND_COLOR, PAINTINGS_CONTENT];
 
 const Home: NextPage = () => {
+  const [accordionState, setAccordionState] = useState(STORY_DATA);
+  const handleStoryClick = () => {
+    setAccordionState(STORY_DATA);
+  }
+  const handleManuscriptsClick = () => {
+    setAccordionState(MANUSCRIPTS_DATA);
+  }
+  const handlePaintingsClick = () => {
+    setAccordionState(PAINTINGS_DATA);
+  }
   return (
     <div className="block">
       <Box sx={{ 
             flexGrow: 1, 
-            display: { xs: 'none', md: 'flex', lg: 'flex' },
             alignContent: 'center'
         }}>
-          <Accordion>
-            <AccordionSummary
-              expandIcon={<ExpandMoreIcon />}
-              aria-controls="panel1a-content"
-              id="panel1a-header"
-              sx = {{
-                width: 600,
-                height: 1000,
-              }}
-            >
-              <ImageTextComponent 
-              imageUrl={STORY_IMAGE}
-              title={STORY_TITLE}
-              // description={STORY_DESCRIPTION}
-              link={STORY_LINK}/>
-            </AccordionSummary>
-            <AccordionDetails>
-              <FeaturedContentComponent 
-                imageUrl={STORY_IMAGE}
-                title={STORY_TITLE}
-                description={STORY_DESCRIPTION}
-                link={STORY_LINK}
-                backgroundColor = {STORY_BACKGROUND_COLOR}
-                content = {STORY_CONTENT}/>
-            </AccordionDetails>
+      <Accordion>
+        <AccordionSummary
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+          className='flex'>
+          <ImageTextComponent
+            imageUrl={STORY_IMAGE}
+            title={STORY_TITLE}
+            // description={STORY_DESCRIPTION}
+            link={STORY_LINK}
+            accordionHandler={handleStoryClick}/>
+          <ImageTextComponent 
+            imageUrl={MANUSCRIPTS_IMAGE}
+            title={MANUSCRIPTS_TITLE}
+            // description={MANUSCRIPTS_DESCRIPTION}
+            link={MANUSCRIPTS_LINK}
+            accordionHandler={handleManuscriptsClick}/>
+          <ImageTextComponent 
+            imageUrl={PAINTINGS_IMAGE}
+            title={PAINTINGS_TITLE}
+            // description={PAINTINGS_DESCRIPTION}
+            link={PAINTINGS_LINK}
+            accordionHandler={handlePaintingsClick}/>
+        </AccordionSummary>
+        <AccordionDetails>
+          <FeaturedContentComponent 
+            imageUrl={accordionState[0]}
+            title={accordionState[1]}
+            description={accordionState[2]}
+            link={accordionState[3]}
+            backgroundColor = {accordionState[4]}
+            content = {accordionState[5]}/>
+        </AccordionDetails>
       </Accordion>
 
-      <Accordion>
+      {/* <Accordion
+        sx= {{
+          width: 1/3
+        }}>
             <AccordionSummary
-              expandIcon={<ExpandMoreIcon />}
               aria-controls="panel1a-content"
               id="panel1a-header"
-              sx = {{
-                width: 600,
-                height: 1000,
-              }}
             >
               <ImageTextComponent 
                 imageUrl={MANUSCRIPTS_IMAGE}
@@ -92,16 +122,14 @@ const Home: NextPage = () => {
                 backgroundColor = {STORY_BACKGROUND_COLOR}
                 content = {STORY_CONTENT}/>
             </AccordionDetails>
-      </Accordion>
-      <Accordion>
+      </Accordion> */}
+      {/* <Accordion
+        sx= {{
+          width: 1/3
+        }}>
             <AccordionSummary
-              expandIcon={<ExpandMoreIcon />}
               aria-controls="panel1a-content"
               id="panel1a-header"
-              sx = {{
-                width: 600,
-                height: 1000,
-              }}
             >
               <ImageTextComponent 
                 imageUrl={PAINTINGS_IMAGE}
@@ -118,7 +146,7 @@ const Home: NextPage = () => {
                 backgroundColor = {STORY_BACKGROUND_COLOR}
                 content = {STORY_CONTENT}/>
             </AccordionDetails>
-      </Accordion>
+      </Accordion> */}
       </Box>
 
       <Box sx={{ 
