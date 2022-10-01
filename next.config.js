e4b0c536ceec8ef74ed42db02159190e65608806
Henok Alem <17439057+henokalem@users.noreply.github.com>
2022-10-01 19:07:26 -0400

    Updates to paintings page

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ['api.bl.uk', 'iiif-cloud.princeton.edu', 'repository.library.brown.edu', 'pemm-data-migration.s3.amazonaws.com'],
+    domains: ['api.bl.uk', 'iiif-cloud.princeton.edu', 'repository.library.brown.edu', 'pemm-data-migration.s3.amazonaws.com', 'ethiopic-manuscripts.s3.amazonaws.com'],
   },
   env: {
     REACT_APP_API: process.env.REACT_APP_API,
--- src/pages/paintings.tsx
+++ src/pages/paintings.tsx
@@ -1,12 +1,8 @@
 import type { NextPage, GetServerSideProps, InferGetServerSidePropsType } from 'next'
-import React, { useEffect, useState } from 'react';
-import ReactPaginate from 'react-paginate';
+import React from 'react';
 import type {Paintings} from '../components/types/paintings';
 import ImagesComponent from '../components/elements/imagesComponent';
-import PaginationButtons from '../components/elements/paginationButtons';
 import axios from 'axios';
-
-const IMAGES_PER_PAGE = 9
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
   try {
@@ -44,47 +40,12 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
 }
 
 const PaintingsPage: NextPage = ({ data }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
-    // We start with an empty list of items.
-    const emptyItems: string[] = [];
-    const [currentItems, setCurrentItems] = useState(emptyItems);
-    const [pageCount, setPageCount] = useState(0);
-    // Here we use item offsets; we could also use page offsets
-    // following the API or data you're working with.
-    const [itemOffset, setItemOffset] = useState(0);
-    useEffect(() => {
-      // Fetch items from another resources.
-      const endOffset = itemOffset + IMAGES_PER_PAGE;
-      setCurrentItems(data.imageUris.slice(itemOffset, endOffset));
-      setPageCount(Math.ceil(data.imageUris.length / IMAGES_PER_PAGE));
-    }, [itemOffset, IMAGES_PER_PAGE]);
-  
-    // Invoke when user click to request another page.
-    const handlePageClick = (event: any) => {
-      const newOffset = (event.selected * IMAGES_PER_PAGE) % data.imageUris.length;
-      setItemOffset(newOffset);
-    };
-
-    const imagesObject: object = {
-      "images": currentItems
-    };
 
   return (
     <div>
         <div className="flex flex-wrap justify-center">
-          <ImagesComponent {...imagesObject} paintings={data.paintings}/>
+          <ImagesComponent images={data.imageUris} paintings={data.paintings}/>
         </div>
-        <div className="ml-20 mr-20">
-            <div id="paginator"></div>
-            <ReactPaginate
-              className="flex bg-gray-400 justify-center"
-              breakLabel="..."
-              nextLabel={PaginationButtons("Next")}
-              onPageChange={handlePageClick}
-              pageRangeDisplayed={5}
-              pageCount={pageCount}
-              previousLabel={PaginationButtons("Previous")}
-            />
-          </div>
     </div>
   )
 }
