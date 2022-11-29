85285d78720ef9d0cfe74dc51f6d1d00027b2825
ayo-gb <70736758+ayo-gb@users.noreply.github.com>
2022-11-29 10:33:05 -0500

    Generated Text Section (Near Completion)
    Properly formatted version with 3 sentences missing/currently being added

--- src/components/elements/generatedManuscriptText.tsx
+++ src/components/elements/generatedManuscriptText.tsx
@@ -3,149 +3,23 @@ import { ElasticInference } from "aws-sdk";
 export const GeneratedManuscriptText = (props: any) => {
     const manuscript = props.manuscript;
     const total_manuscripts_num = 481;
-  
-    // function to determine manuscript age
-    const DeterminemanuscriptAge = (earliest_attest: number) => {
-      let age;
-  
-      if (earliest_attest >= 1300 || earliest_attest < 1500) {
-        age = 'very old';
-      }
-      else if (earliest_attest >= 1500 || earliest_attest < 1800) {
-        age = 'old';
-      }
-      else if (earliest_attest >= 1800 || earliest_attest < 1950) {
-        age = 'recent';
-      }
-      else if (earliest_attest >= 1950) {
-        age = 'very recent';
-      }
-      return age;
-    }
-  
-    // function to convert earliest attestation to earliest GMP
-    const WriteEarliestGMP = (earliest_attest: number) => {
-      let new_format = ((earliest_attest - 1) * 100).toString();
-      return new_format.concat('s');
-    }
-  
-    // function to determine manuscript popularity
-    const DeterminemanuscriptPopularity = (total_records: number) => {
-      let popularity;
-  
-      if (total_records >= 300) {
-        popularity = 'extremely popular';
-      }
-      else if (total_records >= 200) {
-        popularity = 'very popular';
-      }
-      else if (total_records >= 50) {
-        popularity = 'popular';
-      }
-      else if (total_records >= 10) {
-        popularity = 'somewhat popular';
-      }
-      else if (total_records >= 3) {
-        popularity = 'rare';
-      }
-      else {
-        popularity = 'very rare';
-      }
-      return popularity;
-    }
-  
-    // function to write translation availability sentences
-    const ConstructTranslationSentence = (appears_in_french: boolean, appears_in_amharic: boolean,
-      appears_in_latin: boolean, appears_in_italian: boolean, english_translation: string) => {
-  
-      let language_list: string[] = ['Gəˁəz'];
-  
-      if (appears_in_french) {
-        language_list.push('French');
-      }
-      if (appears_in_amharic) {
-        language_list.push('Amharic');
-      }
-      if (appears_in_latin) {
-        language_list.push('Latin');
-      }
-      if (appears_in_italian) {
-        language_list.push('Italian');
-      }
-  
-      // determines if english translation exists
-      if ((english_translation != null) && (english_translation.toLowerCase() != 'summary only')) {
-        language_list.push('English');
-      }
-  
-      return <>This manuscript is available in the following <u>languages</u>: {language_list.join(", ")}. </>
-    }
-  
-    // function to write painting sentences, if any
-    const ConstructPaintingSentences = (total_paintings: number, socum_num: number) => {
-      if (total_paintings === 0) {
-        return <> This manuscript is <u>not illustrated</u> in PEMM manuscripts.</>;
-      }
-      return <> This manuscript is <u>frequently illustrated</u>: it is illustrated in {socum_num} PEMM manuscripts, with a total of {total_paintings} paintings.</>
-    }
-  
-  
-    // function to write illustration availability sentences
-    const ConstructIllustrationSentences = (macomber_id: string, total_paintings: number, socum_num: number) => {
-  
-      let id_list: string[] = ['13', '187', '161', '162', '163', '19', '27', '33', '54',
-        '153', '154', '16', '46', '43', '48', '59', '57', '61', '68', '82', '83', '99', '103', '112',
-        '158', '140', '142', '7', '125', '152', '148', '236'];
-  
-      if (id_list.includes(macomber_id)) {
-        return <> {ConstructPaintingSentences(total_paintings, socum_num)}</>
-      }
-      return <>{ConstructPaintingSentences(total_paintings, socum_num)}</>;
-    }
-  
-    // function to write read aloud sentence, if any
-    const ConstructReadAloudSentence = (readings_date: string) => {
-  
-      if (readings_date != null) {
-        return <>This manuscript is assigned by to <u>read aloud</u> on a particular day during the Ethiopian church service: it is read on a {readings_date}. </>
-      }
-      return;
-    }
-  
-    // function to write life miracle sentence, if any
-    const ConstructLifeMiracleSentence = (type_of_manuscript: string) => {
-  
-      if (type_of_manuscript == 'Life of Mary') {
-        return <>This manuscript is a <u>life miracle</u>: it takes place during Our Lady Mary&apos;s lifetime, not after it.</>
-      }
-      return <>This manuscript is a <u>post-life miracle</u>: it does not take place during Our Lady Mary&apos;s lifetime, but after it.</>;
-    }
-  
-    // function to write manuscript setting sentence, if any
-    const ConstructmanuscriptPlaceSentence = (manuscript_place: string, manuscript_place_type: string) => {
-  
-      if (manuscript_place != null) {
-        return <>This manuscript takes place in {manuscript_place} {(manuscript_place_type != null) ? manuscript_place_type : ''}. </>
-      }
-      return;
-    }
-//-----------
+
     // function to write manuscript setting sentence, if any
     const DetermineManuscriptDateBasis = (manuscript_date_note: string) => {
 
-        if(manuscript_date_note == "Date from ms (colophon)"){
+        if (manuscript_date_note == "Date from ms (colophon)") {
             return <>This date is precise, based on the scribe noting the date in the manuscript. </>
         }
         else if (manuscript_date_note == "Date from king's name") {
             return <>This date is estimated, based on the reigning Ethiopian king's name appearing in the manuscript. </>
         }
-        else if(manuscript_date_note == "Date from ms (paleography)"){
+        else if (manuscript_date_note == "Date from ms (paleography)") {
             return <>This date is estimated, based on paleography (a study of the manuscript's letter shapes). </>
         }
-        else if(manuscript_date_note == "Date from cataloger"){
+        else if (manuscript_date_note == "Date from cataloger") {
             return <>This date is estimated, based on a print or electronic catalog entry about the manuscript. </>
         }
-        else if(manuscript_date_note == "Date from JRB and SGD"){
+        else if (manuscript_date_note == "Date from JRB and SGD") {
             return <>This date is estimated, based on paleography, a study of the manuscript's letter shapes by Jeremy Brown and Stephen Delamarter. </>
         }
 
@@ -154,52 +28,181 @@ export const GeneratedManuscriptText = (props: any) => {
 
     const DetermineTotalStoriesSection = (total_stories: number) => {
 
-        if(total_stories <= 1){
-            if (total_stories != 0){
+        if (total_stories <= 1) {
+            if (total_stories != 0) {
                 return <>This manuscript has only one Marian miracle story. </>
             }
             return
         }
-        else if (total_stories < 24){
-            return <>This manuscript has fewer Marian miracle stories than most, only {total_stories}. </>
+        else if (total_stories < 24) {
+            return <>This manuscript has fewer Marian miracle stories than most, only <b>{total_stories}</b>. </>
         }
-        else if(total_stories < 99){
-            return <>This manuscript has an average number of Marian miracle stories: {total_stories}. </>
+        else if (total_stories < 99) {
+            return <>This manuscript has an average number of Marian miracle stories: <b>{total_stories}</b>. </>
         }
-        else if (total_stories < 199){
-            return <>This manuscript has a high number of Marian miracle stories: {total_stories}.</>
+        else if (total_stories < 199) {
+            return <>This manuscript has a high number of Marian miracle stories: <b>{total_stories}</b>.</>
         }
-        return <>This manuscript has a very high number of Marian miracle stories: {total_stories}. </>
+        return <>This manuscript has a very high number of Marian miracle stories: <b>{total_stories}</b>. </>
     }
+
+    const DeterminePaintingsSentence = (tm_story_paintings: string, total_tm_paintings: number) => {
+
+        if (tm_story_paintings == 'No') {
+            return <>This manuscript has no paintings of Marian miracle stories.</>
+        }
+        else if (tm_story_paintings == 'Yes') {
+            if (total_tm_paintings <= 6) {
+                if (total_tm_paintings > 0) {
+                    return <>This manuscript has a few paintings of Marian miracle stories: <b>{total_tm_paintings}</b>.</>
+                }
+                return
+            }
+            else if (total_tm_paintings < 15) {
+                return <>This manuscript has some paintings of Marian miracle stories: <b>{total_tm_paintings}</b>. </>
+            }
+            else if (total_tm_paintings < 200) {
+                return <>This manuscript has a lot of paintings of Marian miracle stories: <b>{total_tm_paintings}</b>. </>
+            }
+        }
+        else if (tm_story_paintings == 'Related Images') {
+            return <>This manuscript has no paintings of Marian miracle stories, but it does have <b>{total_tm_paintings}</b> paintings of Mary and events in her life. </>
+        }
+        return
+    }
+
+    // GO BACK BC LINK IS NEVER NULL --> ASK FOR NO LINK INDICATOR TO BE STANDARDIZED
+    // GO BACK AND MAKE HYPERLINK DISTINGUISHABLE
+    const DetermineColorPaintingsSentence = (tm_story_paintings: string, scans_of_manuscript_in_color: string, link_to_digital_copy: string) => {
+
+        if (tm_story_paintings == 'Yes' || tm_story_paintings == 'Related Images') {
+            if (scans_of_manuscript_in_color == 'Yes') {
+                if (link_to_digital_copy != null) {
+                    return <>Fortunately, these paintings are digitized and available online in color. To view the manuscript online, go to the
+                        <b><a href={link_to_digital_copy}> digital copy</a></b>.</>
+                }
+                else {
+                    return <>These paintings have been digitized in color but are not available online. Please encourage the repository to put this manuscript online.</>
+                }
+            }
+            else {
+                if (link_to_digital_copy != null) {
+                    return <>These paintings are digitized and available online but only in black and white. To view the manuscript online, go to the
+                        <b><a href={link_to_digital_copy}> digital copy</a></b>.</>
+                }
+            }
+        }
+        return
+    }
+
+    // GO BACK AND FIX "AND" and "," LOGIC
+    const DetermineFoliosSentence = (total_folios: number, total_pages: number, total_scans: number, columns_per_page: number, line_range_per_column: string, folio_start_of_the_tm_part: string, scan_start_of_the_tm_part: string) => {
+        let folios = null
+        let pages = null
+        let scans = null
+        let columns = null
+        let lines = null
+        let folio_start = null
+
+        if (total_folios != null) {
+            folios = <> <b>{total_folios}</b> folios</>
+        }
+
+        if (total_pages != null) {
+            pages = <>, <b>{total_pages}</b> pages</>
+        }
+
+        if (total_scans != null) {
+            scans = <>, and <b>{total_scans}</b> scans</>
+        }
+
+        if (columns_per_page != null) {
+            columns = <><b>{columns_per_page}</b> columns per page</>
+        }
+
+        if (line_range_per_column != null) {
+            lines = <> and approximately <b>{line_range_per_column}</b> lines per column</>
+        }
+
+        if (folio_start_of_the_tm_part != null) {
+            folio_start = <> The Marian miracle stories begin on folio <b>{folio_start_of_the_tm_part}</b> of the whole manuscript.</>
+        }
+
+        return <>This manuscript has a total of {folios}{pages}{scans}. It has {columns} {lines}. {folio_start}</>
+    }
+
+    // duplicate/missing_scans/rebound_in_disorder= COLUMN DOESNT EXIST IN SHEET --> USE UNDERSCORES AND NOT "/"
+    const DeterminePageProblemSentence = (disordered_rebound: string, duplicate_or_missing: string) => {
+        if (disordered_rebound != null && duplicate_or_missing != null) {
+            return <> Unfortunately, this manuscript was bound in disorder and digitized improperly so that some folios were skipped or duplicated.</>
+        }
+        else if (disordered_rebound != null) {
+            return <> Unfortunately, this manuscript was bound in disorder.</>
+        }
+        else if (duplicate_or_missing != null) {
+            return <> Unfortunately, this manuscript was digitized improperly and some folios were skipped or duplicated.</>
+        }
+        return
+    }
+
+    // SEE TICKET ABOUT HYPERLINK TO STAFF PAGE
+    const DetermineCatalogerSentence = (pemm_cataloger_name: string, print_cataloger_name: string, pemm_volunteer_name: string) => {
+        let s1 = null
+        let s2 = null
+        if (pemm_cataloger_name != null && print_cataloger_name != null) {
+            s1 = <> The stories in this manuscript were cataloged by <b>{pemm_cataloger_name}</b> and <b>{print_cataloger_name}</b>.</>
+        }
+        else {
+            if (pemm_cataloger_name != null) {
+                s1 = <> The stories in this manuscript were cataloged by <b>{pemm_cataloger_name}</b></>
+            }
+            else if (print_cataloger_name != null) {
+                s1 = <> The stories in this manuscript were cataloged by <b>{print_cataloger_name}</b></>
+            }
+        }
+
+        if (pemm_volunteer_name != null) {
+            s2 = <> <b>{pemm_volunteer_name}</b> provided assistance by typing incipits.</>
+        }
+
+        return <> {s1} {s2}</>
+    }
+
     // parts n, m, p instructions are missing from issue post
     // WHERE TO GET UPLOAD DATE + TOTAL NUM OF MANUSCRIPTS
     return (
-      <>
-        <div>
-        <h2>
-            This <u>{manuscript.language}</u> language manuscript was created between {manuscript.date_range_start} and {manuscript.date_range_end}. 
-            {DetermineManuscriptDateBasis(manuscript.date_note)}
-        </h2>
-        <h2>
-            This manuscript was cataloged, digitized, or purchased in (and thus may have been created in or near) [CANT FIND PROVENANCE COLUMN] in [OR PLACE_RECORDED COLUMN].
-        </h2>
-        <h2>
-            {DetermineTotalStoriesSection(manuscript.total_stories)}
-        </h2>
-          <h2 style={{textIndent:"20px"}}>This manuscript is <u>{DeterminemanuscriptAge(manuscript.earliest_attestation)}</u>: the earliest PEMM manuscript<sup>1</sup> in which this manuscript appears is from&nbsp; 
-            {manuscript.earliest_attestation}. </h2>
-          <h2 style={{textIndent:"20px"}}>This manuscript is <u>{DeterminemanuscriptPopularity(manuscript.total_records)}</u>: appearing in {Math.round((manuscript.total_records / total_manuscripts_num) * 100)}%
-            of PEMM manuscripts with five stories or more.</h2>
-          <h2 style={{textIndent:"20px"}}>{ConstructIllustrationSentences(manuscript.macomber_id, manuscript.total_paintings, manuscript.sum_of_countif_unique_manuscript)} </h2>
-          <h2 style={{textIndent:"20px"}}>{ConstructLifeMiracleSentence(manuscript.type_of_manuscript)} </h2>
-          <h2 style={{textIndent:"20px"}}>This manuscript was originally <u>composed</u> in {manuscript.origin}. </h2>
-          <h2 style={{textIndent:"20px"}}>{ConstructReadAloudSentence(manuscript.readings_dates)} </h2>
-          <h2 style={{textIndent:"20px"}}>{ConstructTranslationSentence(manuscript.appears_in_french, manuscript.appears_in_amharic, manuscript.appears_in_latin, manuscript.appears_in_italian, manuscript.english_translation)} </h2>
-          <h2 style={{textIndent:"20px"}}>{ConstructmanuscriptPlaceSentence(manuscript.canonical_manuscript_place, manuscript.canonical_manuscript_place_type)} </h2>
-        </div>
-        <div style={{ marginTop: "10px", textIndent:"20px" }}>
-          <small ><sup>1</sup> A &quot;PEMM manuscript&quot; is defined as any Gəˁəz Marian manuscript that PEMM has catalogued. For more information, see About page.</small>
-        </div>
-      </>
+        <>
+            <div>
+                <h2>
+                    This <b>{manuscript.language}</b> language manuscript was created between <b>{manuscript.date_range_start}</b> and <b>{manuscript.date_range_end}</b>.
+                    {DetermineManuscriptDateBasis(manuscript.date_note)}
+                </h2>
+                <h2>
+                    This manuscript was cataloged, digitized, or purchased in (and thus may have been created in or near) [CANT FIND PROVENANCE COLUMN] in [OR PLACE_RECORDED COLUMN].
+                </h2>
+                <h2>
+                    {DetermineTotalStoriesSection(manuscript.total_stories)}
+                </h2>
+                <h2>
+                    {DeterminePaintingsSentence(manuscript.tm_story_paintings, manuscript.total_tm_paintings)}
+                </h2>
+                <h2>
+                    {DetermineColorPaintingsSentence(manuscript.tm_story_paintings, manuscript.scans_of_manuscript_in_color, manuscript.link_to_digital_copy)}
+                </h2>
+                <h2>
+                    {DetermineFoliosSentence(manuscript.total_folios, manuscript.total_pages, manuscript.total_scans, manuscript.columns_per_page, manuscript.line_range_per_column, manuscript.folio_start_of_the_tm_part, "")}
+                </h2>
+                <h2>
+                    {DeterminePageProblemSentence(manuscript.mss_rebound_in_disorder_or_there_are_breaks_in_the_sequence_of_tm, manuscript.duplicate_missing_scans_rebound_in_disorder)}
+                </h2>
+                <h2>
+                    {DetermineCatalogerSentence(manuscript.pemm_cataloger_name, manuscript.print_cataloger_name, manuscript.pemm_volunteer_name)}
+                </h2>
+                <h2>
+                    {manuscript.catalog != null ? <> This manuscript has a print catalog: <b>{manuscript.catalog}</b></> : <></>}
+                </h2>
+
+            </div>
+        </>
     );
-  };
+};
--- src/data/manuscript1.ts
+++ src/data/manuscript1.ts
@@ -10,6 +10,21 @@ export const MANUSCRIPT_1_TEST_DATA: Manuscripts = {
     "date_note": "Date from king's name",
     "total_stories": 76,
     "total_tm_paintings": 2,
-    "scans_of_manuscript_in_color": true,
-    
+    "scans_of_manuscript_in_color": 'Yes',
+    "tm_story_paintings": 'Related Images',
+    "link_to_digital_copy": 'https://www.loc.gov/item/2021356021/',
+    "total_folios": 203,
+    "total_pages": 203,
+    "total_scans": 410,
+    "columns_per_page": 2,
+    "line_range_per_column": '16',
+    "folio_start_of_the_tm_part": '9r',
+    "scan_start_of_the_tm_part": '9b',
+   "mss_rebound_in_disorder_or_there_are_breaks_in_the_sequence_of_tm": 'placeholder',
+   "duplicate_missing_scans_rebound_in_disorder": 'placeholder',
+   "pemm_cataloger_name": "Jeremy R. Brown", 
+   "print_cataloger_name": "E.A.W. Budge and W.F. Macomber", 
+   "pemm_volunteer_name": "Chiara Lombardi",
+   "catalog": "Dillmann, C. F. A. 1847. Catalogus codicum manuscriptorum orientalium qui in Museo Britannico asservantur, III: Codices Aethiopicos amplectens (n.p.: E Museo Britannico, 1847) , page 48ab"
+
   };
