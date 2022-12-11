42a79b4621a4bfc0e81baa76078e0e9b9ef296cc
Henok Alem <17439057+henokalem@users.noreply.github.com>
2022-12-11 15:57:02 -0500

    Adding manuscripts index api

--- src/components/types/manuscripts.ts
+++ src/components/types/manuscripts.ts
@@ -1,4 +1,10 @@
 export type Manuscripts = {
     manuscript_id?: number | null,
-    manuscript_name?: string | null
-  }
+    manuscript_name?: string | null,
+    manuscript_full_name?: string | null,
+    century?: string | null,
+    total_stories?: number | null,
+    provenance?: string | null,
+    total_paintings?: number | null,
+    link_to_digital_copy?: string | null
+}
