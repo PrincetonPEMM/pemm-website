ad4b075f64a48e1e1fde89f32109c3b9ce2a0cb2
Pak Hei Li <67703799+issacjohannli@users.noreply.github.com>
2023-04-11 19:51:51 +0100

    Formatted code and added cataloged miracle records component

--- src/pages/manuscripts/[manuscriptId].tsx
+++ src/pages/manuscripts/[manuscriptId].tsx
@@ -1,59 +1,62 @@
 import type { NextPage, GetServerSideProps, InferGetServerSidePropsType } from 'next'
 import Box from '@mui/material/Box';
-import type {Manuscripts} from '../../components/types/manuscripts';
+import type { Manuscripts } from '../../components/types/manuscripts';
 import { MANUSCRIPT_1_TEST_DATA } from '../../data/manuscript1';
+import ManuscriptCatalogedMiracleRecords from "../../components/elements/manuscriptCatalogedMiracleRecords";
 import Typography from '@mui/material/Typography';
 import axios from 'axios';
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
   const { manuscriptId } = context.query;
-    try {
-      var manuscript: Manuscripts = {};
-      if (process.env['ENVIRONMENT'] == "DEV") {
-        manuscript = MANUSCRIPT_1_TEST_DATA;
-      } else {
-        const res = await axios(process.env.REACT_APP_API + 'manuscripts/' + manuscriptId);
-        if (res.data.length > 0) {
-          manuscript = res.data[0];
-        }
+  try {
+    var manuscript: Manuscripts = {};
+    if (process.env['ENVIRONMENT'] == "DEV") {
+      manuscript = MANUSCRIPT_1_TEST_DATA;
+    } else {
+      const res = await axios(process.env.REACT_APP_API + 'manuscripts/' + manuscriptId);
+      if (res.data.length > 0) {
+        manuscript = res.data[0];
       }
+    }
 
-      return {
-        props: {
-          data: {
-            manuscript: manuscript
-          }
+    return {
+      props: {
+        data: {
+          manuscript: manuscript
         }
       }
-    } catch {
-        return {
-          props: {
-            data: {
-              manuscript: {}
-            }
-          }
+    }
+  } catch {
+    return {
+      props: {
+        data: {
+          manuscript: {}
         }
+      }
     }
   }
+}
 
 const ManuscriptDetailPage: NextPage = ({ data }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+  console.log(data.manuscript);
   return (
     <div>
-      <Box sx={{ 
-        flexGrow: 1, 
+      <Box sx={{
+        flexGrow: 1,
         display: { xs: 'none', md: 'flex', lg: 'flex' }
-        }}>
+      }}>
         <div className='m-4 w-3/4'>
           <Typography variant="h3">{data.manuscript.manuscript_name}</Typography>
+          <ManuscriptCatalogedMiracleRecords manuscript={data.manuscript} />
         </div>
       </Box>
-      <Box sx={{ 
-            flexGrow: 1, 
-            display: { xs: 'flex', md: 'none', lg: 'none' }
-        }}>
-          <div className='m-4'>
-            <Typography variant="h6">{data.manuscript.manuscript_name}</Typography>
-          </div>
+      <Box sx={{
+        flexGrow: 1,
+        display: { xs: 'flex', md: 'none', lg: 'none' }
+      }}>
+        <div className='m-4'>
+          <Typography variant="h6">{data.manuscript.manuscript_name}</Typography>
+        </div>
       </Box>
     </div>
   )
