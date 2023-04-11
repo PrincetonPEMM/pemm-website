cb35bafbe2e00da2480beeb47afebf7169d3011f
Pak Hei Li <67703799+issacjohannli@users.noreply.github.com>
2023-04-11 19:44:15 +0100

    Add story_instances array for manuscript type

--- src/components/types/manuscripts.ts
+++ src/components/types/manuscripts.ts
@@ -1,10 +1,13 @@
+import { Instances } from "./instances";
+
 export type Manuscripts = {
-    manuscript_id?: number | null,
-    manuscript_name?: string | null,
-    manuscript_full_name?: string | null,
-    century?: string | null,
-    total_stories?: number | null,
-    provenance?: string | null,
-    total_paintings?: string | null,
-    link_to_digital_copy?: string | null
-}
+    manuscript_id?: number | null;
+    manuscript_name?: string | null;
+    manuscript_full_name?: string | null;
+    century?: string | null;
+    total_stories?: number | null;
+    provenance?: string | null;
+    total_paintings?: string | null;
+    link_to_digital_copy?: string | null;
+    story_instances?: Instances[] | null;
+};
